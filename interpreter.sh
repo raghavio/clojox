e5069834fa67fa8d115018b6a4ea0d056aa06131
Raghav <raghav_ftw@hotmail.com>
2024-06-02 04:09:22 +0530

    Make the path relative to run uberjar

--- interpreter.sh
+++ interpreter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the JAR file with Java options
-java --enable-preview -jar /home/raghav/Code/Github/clojox/target/uberjar/clojox-0.1.0-SNAPSHOT-standalone.jar "$@"
+java --enable-preview -jar ./target/uberjar/clojox-0.1.0-SNAPSHOT-standalone.jar "$@"
